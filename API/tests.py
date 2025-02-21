9f1921ff2f31d36b563d61e55eef3c4dc96c309d
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-02-21 09:38:26 +0100

    writing tests

--- API/tests.py
+++ API/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 # Create your tests here.
+
+class APITests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('upload')
+
+    def test_invalid_method(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.json(), {'error': 'invalid HTTP method'})
+
+    def test_invalid_type_file_post(self):
+        file = SimpleUploadedFile(
+            'malware.exe',
+            b'malware',
+            content_type='application/x-msdownload'
+        )
+        response = self.client.post(self.url, {'file': file})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'error': 'invalid file type'})
