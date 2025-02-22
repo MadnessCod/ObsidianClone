9768c66b48a7b9e2c7cd2cd063f942a3798cf4ca
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-02-22 18:32:17 +0100

    created File model, fixed admin not installed

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
+
+from .models import File
 # Register your models here.
+
+@register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'file_name']
+    list_filter = ['created_at', 'updated_at']
+
+    def file_name(self, obj):
+        return obj.file.name.split('/')[-1]
+
+    file_name.short_description = 'File Name'
--- API/models.py
+++ API/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class MyBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return NotImplemented('str method not implemented')
+
+
+class File(MyBaseModel):
+    file = models.FileField(upload_to='files/')
+
+    def  __str__(self):
+        return self.file.name
+
+    class Meta:
+        ordering = ['-updated_at']
