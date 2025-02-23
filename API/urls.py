6f2a46d06242e08de76cbc9402ba80eea94175f2
Sadra Hosseini <sadrahosseini98@gmail.com>
2025-02-23 20:42:39 +0100

    views and forms implementation

--- API/urls.py
+++ API/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from .views import UploadView, FileListView
 
 urlpatterns = [
+    path('upload/', UploadView.as_view(), name='upload'),
+    path('file/', FileListView.as_view(), name='file_list'),
 ]
--- API/views.py
+++ API/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import File
+from .forms import FileForm
 
 # Create your views here.
+
+class UploadView(CreateView):
+    model = File
+    form_class = FileForm
+    success_url = reverse_lazy('file_list')
+
+
+class FileListView(ListView):
+    model = File
+    context_object_name = 'file'
+
+
